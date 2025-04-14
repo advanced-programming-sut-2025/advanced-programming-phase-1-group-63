3c7b50ef85a192394274112117cbb8f9f5299566
seyed-hasan <seyyed.hasan.khb@gmail.com>
2025-04-14 19:19:37 +0330

    taghriban final commit for phase0

--- src/controller/TradeMenuController.java
+++ src/controller/TradeMenuController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import model.Item;
+import model.Result;
+import model.Trade;
+import model.User;
+import model.enums.TradeType;
+
 public class TradeMenuController extends GenerallAppController {
+    public Result trade (User firstUser, User secondUser, TradeType type, Item item, int amount, int price) {
+
+    }
+
+    public Result trade (User user, User targetUser, TradeType type, Item item, int amount, Item targetItem, int targetAmount) {
+
+    }
+
+    public Result tradeResponse (User user, Trade trade) {
+
+    }
 }
