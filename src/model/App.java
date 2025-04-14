7c8023270cadde804df50bb6d6eb764552782bb1
seyed-hasan <seyyed.hasan.khb@gmail.com>
2025-04-14 13:18:26 +0330

    Edit structure 2

--- src/model/App.java
+++ src/model/App.java
@@ -1,10 +1,33 @@
 package model;
 
+import model.enums.Menu;
+
 import java.util.ArrayList;
 
 public class App {
     private ArrayList<User> users;
     private User loggedInUser;
+    private Menu currentMenu;
+    private static App instance;
+
+    public App() {
+
+    }
+
+    public static App getInstance() {
+        if (instance == null)
+            instance = new App();
+        return instance;
+    }
+
+    public Menu getCurrentMenu() {
+        return currentMenu;
+    }
+
+    public void setCurrentMenu(Menu currentMenu) {
+        this.currentMenu = currentMenu;
+    }
+
 
     public ArrayList<User> getUsers() {
         return users;
