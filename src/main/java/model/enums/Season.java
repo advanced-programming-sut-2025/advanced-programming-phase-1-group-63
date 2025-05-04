107a233217c73e1a2c2a86a57e90c5627ad8f925
seyedmmd <mester313000@gmail.com>
2025-05-04 20:13:35 +0330

    fix: connecting season to weather

--- src/main/java/model/enums/Season.java
+++ src/main/java/model/enums/Season.java
@@ -1,18 +1,22 @@
 package main.java.model.enums;
 
+import static main.java.model.enums.Weather.*;
+
 public enum Season {
-    Spring(0),
-    Summer(1),
-    Fall(2),
-    Winter(3);
+    Spring(0, new Weather[]{Sunny, Rain, Storm}),
+    Summer(1, new Weather[]{Sunny, Rain, Storm}),
+    Fall(2, new Weather[]{Sunny, Rain, Storm}),
+    Winter(3, new Weather[]{Sunny,Snow});
 
     int seasonNumber;
+    Weather[] weathers;
 
-    Season(int seasonNumber) {
+    Season(int seasonNumber, Weather[] weathers) {
         this.seasonNumber = seasonNumber;
+        this.weathers = weathers;
     }
 
     public Season nextSeason() {
-
+        
     }
 }
