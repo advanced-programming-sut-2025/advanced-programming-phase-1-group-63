744abe2732d561c61883030414fe5a5b3288a519
seyed-hasan <seyyed.hasan.khb@gmail.com>
2025-05-16 00:17:56 +0330

    commit!

---
+++ NewProject/Project/src/main/java/controller/main/GameMainMenuController.java
@@ -0,0 +1,228 @@
+package controller.main;
+
+import model.App;
+import model.Result;
+
+public class GameMainMenuController { //extends GeneralAppController {
+    public Result exitGame(App app) {
+    }
+
+    public Result deleteGame(App app) {
+    }
+
+    public Result nextTurn(App app) {
+    }
+
+    public Result walk(App app, String x, String y) {
+    }
+
+    public Result printMap(App app, String x, String y, String size) {
+    }
+
+    public Result helpReadMap(App app) {
+    }
+
+    public Result showEnergy(App app) {
+    }
+
+    public Result showTime(App app) {
+    }
+
+    public Result showDate(App app) {
+    }
+
+    public Result showDateTime(App app) {
+    }
+
+    public Result showWeekDay(App app) {
+    }
+
+    public Result showSeason(App app) {
+    }
+
+    public Result showWeather(App app) {
+    }
+
+    public Result showTomorrowWeather(App app) {
+    }
+
+    public Result greenhouseBuild(App app) {
+    }
+
+    public Result showInventory(App app) {
+    }
+
+    public Result deleteInventoryItem(App app, String itemName, String number) {
+    }
+
+    public Result toolsEquip(App app, String toolName) {
+    }
+
+    public Result showCurrentTool(App app) {
+    }
+
+    public Result showToolsList(App app) {
+    }
+
+    public Result upgradeTool(App app, String toolName) {
+    }
+
+    public Result useTool(App app, String direction) {
+    }
+
+    public Result showCraftInfo(App app, String craftName) {
+    }
+
+    public Result plant(App app, String seed, String direction) {
+    }
+
+    public Result showPlant(App app, String x, String y) {
+    }
+
+    public Result fertilize(App app, String fertilizer, String direction) {
+    }
+
+    public Result showWater(App app) {
+    }
+
+    public Result showCraftRecipes(App app) {
+    }
+
+    public Result crafting(App app, String itemName) {
+    }
+
+    public Result placeItem(App app, String itemName, String direction) {
+    }
+
+    public Result putRefrigerator(App app, String item) {
+    }
+
+    public Result pickRefrigerator(App app, String item) {
+    }
+
+    public Result showCookRecipes(App app) {
+    }
+
+    public Result prepareCooking(App app, String recipeName) {
+    }
+
+    public Result eat(App app, String foodName) {
+    }
+
+    public Result artisanUse(App app, String artisanName, String itemName) {
+    }
+
+    public Result artisanGet(App app, String artisanName) {
+    }
+
+    public Result sellProduct(App app, String productName, String count) {
+    }
+
+    public Result startBuy(App app) {
+    }
+
+    public Result build(App app, String name, String x, String y) {
+    }
+
+    public Result buyAnimal(App app, String animal, String name) {
+    }
+
+    public Result pet(App app, String name) {
+    }
+
+    public Result showAnimals(App app) {
+    }
+
+    public Result shepherdAnimal(App app, String name, String x, String y) {
+    }
+
+    public Result feedHay(App app, String name) {
+    }
+
+    public Result showProduces(App app) {
+    }
+
+    public Result collectProduce(App app, String name) {
+    }
+
+    public Result sellAnimal(App app, String name) {
+    }
+
+    public Result fishing(App app, String pole) {
+    }
+
+    public Result showFriendships(App app) {
+    }
+
+    public Result talk(App app, String username, String message) {
+    }
+
+    public Result talkHistory(App app, String username) {
+    }
+
+    public Result gift(App app, String username, String item, String count) {
+    }
+
+    public Result showGiftList(App app) {
+    }
+
+    public Result giftRate(App app, String giftID, String rate) {
+    }
+
+    public Result giftHistory(App app, String username) {
+    }
+
+    public Result hug(App app, String username) {
+    }
+
+    public Result flower(App app, String username) {
+    }
+
+    public Result askMarriage(App app, String username, String ring) {
+    }
+
+    public Result respondMarriage(App app, String accepting, String username) {
+    }
+
+    public Result meetNPC(App app, String name) {
+    }
+
+    public Result giftNPC(App app, String name, String item) {
+    }
+
+    public Result showNPCFriendship(App app) {
+    }
+
+    public Result showQuestsList(App app) {
+    }
+
+    public Result finishQuest(App app, String index) {
+    }
+
+    public Result cheatTime(App app, String hour) {
+    }
+
+    public Result cheatDate(App app, String day) {
+    }
+
+    public Result cheatThor(App app, String x, String y) {
+    }
+
+    public Result cheatWeather(App app, String type) {
+    }
+
+    public Result cheatEnergy(App app, String value) {
+    }
+
+    public Result cheatEnergyLimit(App app) {
+    }
+
+    public Result cheatItem(App app, String itemName, String count) {
+    }
+
+    public Result cheatAnimalFriendship(App app, String animalName, String amount) {
+    }
+
+    public Result cheatBalance(App app, String count) {
+    }
+}
